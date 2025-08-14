a84ae960b02de96ef5f8359506c7630de7dd41a4
hdxestudart <erico.studartdf@gmail.com>
2025-08-14 13:08:32 -0300

    feat: add home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,34 @@
-function Home() {
-    return <div>Hello World</div>
-}
+import { useEffect, useState } from "react";
 
-export default Home;
+import axios from "axios";
+
+import ProductCard from "../components/ProductCard";
+
+
+export default function Home() {
+    const [products, setProducts] = useState([]);
+    const [choosedCard, setChoosedCard] = useState("");
+
+    useEffect(() => {
+        async function fetchProducts() {
+            const result = await axios.get(
+                'https://fakestoreapi.in/api/products'
+            );
+            const products = result.data.products
+            console.log(products);
+            setProducts(products);
+        };
+        fetchProducts();
+    }, []);
+
+    return (
+        <>
+            <h1 className="page-title">Products Store</h1>
+            <div className="cards">
+                {products.map((product) => (
+                    <ProductCard key={product.id} product={product}/>
+                ))}
+            </div>
+        </>
+    )
+}
