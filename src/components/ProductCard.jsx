4996c49693b4748a448c3d3a65c39cbbb872ccb2
hdxestudart <erico.studartdf@gmail.com>
2025-08-14 19:49:29 -0300

    feat: add styling to product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,19 @@
-export default function ProductCard ( {product} ) {
+export default function ProductCard ( {product, onSelectedProduct} ) {
     return (
-        <div className="card">
-            <div className="card-info">
-                <h3>Product: {product.title}</h3>
-                <img src={product.image}></img>
-                <div>Price: {product.price}</div>
+        <div className="flex itens-center justify-center max-w-[30%] p-[1.5%] border-2 border-white-600 rounded-md text-[14px] w-full  bg-slate-200" onClick={() => onSelectedProduct(product)}>
+            <div className="flex flex-col justify-between h-full text-yellow-950 font-semibold">
+                <img className="rounded-md self-center m-2" src={product.image}></img>
+                <h3 className="text-left">{product.title}</h3>
+                <div className="flex flex-col">
+                    <div className="flex flex-row mt-2">
+                        <h3>Price: U${product.price}.00</h3>
+                        <div className="text-red-500">
+                            {product.discount && `\u00A0-${product.discount}%`}
+                        </div>
+                    </div>
+                    <div>Model: {product.model}</div>
+                    <div>Brand: {product.brand}</div>
+                </div>
             </div>
         </div>
     );
